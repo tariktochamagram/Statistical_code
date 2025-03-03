535a7c2ad096f9aa5271c746e947b6c430a1a5dc
tariktochamagram <tarik2banbeis@yahoo.com>
2025-03-04 00:27:50 +0600

    mke it final

---
+++ day_01.R
@@ -0,0 +1,47 @@
+# Day 1 R script
+# Basic commands
+print("Hello World!")
+
+6+3
+3^3
+
+
+22^2+7
+22^(2+7)
+
+answer<- 2+2
+answer
+
+answer2<-(3.5+1)^2
+answer+answer2
+
+answer3<-answer/answer2
+answer3
+
+log(27)
+
+abs(-9)
+
+x<-sqrt(2.5)
+exp((x*2)^(1/3))
+
+round(14.5378, digits=2)
+
+round(14.5378, digits=1)
+
+logb(27, base=3.5)
+
+signif(3.1415926, digits=3)
+
+signif(3.1415926, 3)
+
+
+
+# Data types
+
+# By default, R considers a number as double
+x<-8.1456
+typeof(x)
+
+y<-4
+typeof(y)
